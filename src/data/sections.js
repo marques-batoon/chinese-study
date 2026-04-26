af72d56db2ab545fbe6e96429e8612767e6e345a
marques-batoon <mbatoon1996@gmail.com>
2026-04-25 17:14:46 -1000

    added new sections and vocabulary

--- src/data/sections.js
+++ src/data/sections.js
@@ -4,6 +4,26 @@ export const sections = [
     label: "Phrases",
     entryIds: [1, 2, 3, 112, 113, 114],
   },
+  {
+    id: "questions",
+    label: "Questions",
+    entryIds: [44, 54, 71, 80,102, 103, 104],
+  },
+  {
+    id: "verbs",
+    label: "Verbs",
+    entryIds: [10, 26, 27, 28, 38, 52, 66, 72, 78, 98, 105, 108, 115, 116, 117, 118, 119, 120],
+  },
+  {
+    id: "colors",
+    label: "Colors",
+    entryIds: [83, 70, 121, 122, 123, 124, 125, 126, 127, 128],
+  },
+  {
+    id: "tastes",
+    label: "Taste words",
+    entryIds: [129, 130, 131, 132, 133],
+  },
   // Add new sections here:
   // { id: "unique-id", label: "Display Name", entryIds: [4, 5, 6] },
 ];
