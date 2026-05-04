ecaa812340a77cf290681335255e42a4fd92c799
marques-batoon <mbatoon1996@gmail.com>
2026-05-04 10:08:47 -1000

    added vocab and new section question words

--- src/data/sections.js
+++ src/data/sections.js
@@ -12,7 +12,7 @@ export const sections = [
   {
     id: "verbs",
     label: "Verbs",
-    entryIds: [10, 26, 27, 28, 38, 52, 66, 72, 78, 98, 105, 108, 115, 116, 117, 118, 119, 120],
+    entryIds: [10, 26, 27, 28, 38, 52, 66, 72, 78, 98, 105, 108, 115, 116, 117, 118, 119, 120, 146, 150, 153, 158],
   },
   {
     id: "colors",
@@ -23,6 +23,16 @@ export const sections = [
     id: "tastes",
     label: "Taste words",
     entryIds: [129, 130, 131, 132, 133],
+  },
+  {
+    id: "food",
+    label: "Food",
+    entryIds: [4, 18, 68, 88, 134, 135, 136, 137, 138],
+  },
+  {
+    id: "question-words",
+    label: "Question words",
+    entryIds: [44, 54, 71, 80, 103, 104, 155, 156, 157],
   },
   // Add new sections here:
   // { id: "unique-id", label: "Display Name", entryIds: [4, 5, 6] },
